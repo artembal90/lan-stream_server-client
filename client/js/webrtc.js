644f5b472bc96be339518ee12737186ae9e5b28a
artembal90 <artembal90@gmail.com>
2026-09-01 03:20:22 +0300

    Complete browser ICE gathering before SDP offer

--- client/js/webrtc.js
+++ client/js/webrtc.js
@@ -1,14 +1,21 @@
-export function createPeerConnection(onTrack) {
-  const pc = new RTCPeerConnection({
-    iceServers: [],
+function waitForIceGatheringComplete(pc) {
+  if (pc.iceGatheringState === 'complete') return Promise.resolve();
+  return new Promise((resolve) => {
+    const check = () => {
+      if (pc.iceGatheringState === 'complete') {
+        pc.removeEventListener('icegatheringstatechange', check);
+        resolve();
+      }
+    };
+    pc.addEventListener('icegatheringstatechange', check);
   });
+}
 
+export function createPeerConnection(onTrack) {
+  const pc = new RTCPeerConnection({ iceServers: [] });
   pc.ontrack = (event) => {
-    if (event.streams.length > 0) {
-      onTrack(event.streams[0]);
-    }
+    if (event.streams.length > 0) onTrack(event.streams[0]);
   };
-
   return pc;
 }
 
@@ -16,5 +23,6 @@ export async function createViewerOffer(pc) {
   pc.addTransceiver('video', { direction: 'recvonly' });
   const offer = await pc.createOffer();
   await pc.setLocalDescription(offer);
+  await waitForIceGatheringComplete(pc);
   return pc.localDescription;
 }
